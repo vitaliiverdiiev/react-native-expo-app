69c1548467e498aacb55475dcbfa448643cf4a95
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-07-17 14:25:04 +0300

    figuring out the flow

--- App.tsx
+++ App.tsx
@@ -1,23 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hekko Mleko</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-around",
+    backgroundColor: "ghostwhite",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
   },
-  text: {
+  box: {
+    width: 300,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategray",
+  },
+  boxText: {
+    color: "darkslategray",
     fontWeight: "bold",
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#3</Text>
+      </View>
+    </View>
+  );
+}
